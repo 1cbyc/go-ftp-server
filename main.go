cf9e9b63c41393bec748e86f4e30c64e600ff35f
1cbyc <isaacnsisong@gmail.com>
2025-07-20 13:53:08 -0700

    my third cintentional did with go

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mrinalxdev/go-ftp-server/internal/config"
+	"github.com/mrinalxdev/go-ftp-server/internal/server"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	configFile = flag.String("config", "config.yaml", "Path to configuration file")
+	port       = flag.Int("port", 2121, "Port to listen on")
+	host       = flag.String("host", "localhost", "Host to bind to")
+	rootDir    = flag.String("root", "./ftp_root", "Root directory for FTP server")
+	verbose    = flag.Bool("verbose", false, "Enable verbose logging")
+)
+
+func main() {
+	flag.Parse()
+
+	// Setup logging
+	logLevel := logrus.InfoLevel
+	if *verbose {
+		logLevel = logrus.DebugLevel
+	}
+	logrus.SetLevel(logLevel)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// Load configuration
+	cfg, err := config.Load(*configFile)
+	if err != nil {
+		logrus.Warnf("Failed to load config file %s: %v, using defaults", *configFile, err)
+		cfg = &config.Config{
+			Server: config.ServerConfig{
+				Host: *host,
+				Port: *port,
+			},
+			FTP: config.FTPConfig{
+				RootDir: *rootDir,
+			},
+		}
+	}
+
+	// Override config with command line flags if provided
+	if *host != "localhost" {
+		cfg.Server.Host = *host
+	}
+	if *port != 2121 {
+		cfg.Server.Port = *port
+	}
+	if *rootDir != "./ftp_root" {
+		cfg.FTP.RootDir = *rootDir
+	}
+
+	// Create and start server
+	srv := server.NewFTPServer(cfg)
+
+	// Setup graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigChan
+		logrus.Infof("Received signal %v, shutting down gracefully...", sig)
+		srv.Shutdown()
+	}()
+
+	// Start the server
+	logrus.Infof("Starting FTP server on %s:%d", cfg.Server.Host, cfg.Server.Port)
+	logrus.Infof("Root directory: %s", cfg.FTP.RootDir)
+
+	if err := srv.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
